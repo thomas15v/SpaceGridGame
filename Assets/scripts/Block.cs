68aec5ca7e1fc2262f6e03c4680c14aac42a1c04
Thomas Vanmellaerts <tvanmellaerts@gmail.com>
2017-08-03 20:05:26 +0200

    First Commit

---
+++ Assets/scripts/Block.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour
+{
+
+	public BlockConfig config;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+[Serializable]
+public class BlockConfig
+{
+	public Image baseImage;
+	
+	public Image leftOpenImage;
+	public Image rightOpenImage;
+	public Image topOpenImage;
+	public Image bottomOpenImage;
+	
+	public Image leftClosedImage;
+	public Image rightClosedImage;
+	public Image topClosedImage;
+	public Image bottomClosedImage;
+	
+}
---
+++ Assets/scripts/EditorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class EditorController : MonoBehaviour {
+	
+	private string blockPath = Path.Combine(Application.streamingAssetsPath, "Blocks");
+
+	// Use this for initialization
+	void Start () {
+		foreach (var directory in Directory.GetDirectories(blockPath))
+		{
+			var blockConfig = Path.Combine(directory, "block.json");
+			if (File.Exists(blockConfig))
+			{
+				
+			}
+			
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
